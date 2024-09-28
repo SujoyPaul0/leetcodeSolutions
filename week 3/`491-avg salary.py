ae0cbd73993070a743c090a4298e448d2bad6227
Sujoy Paul <sujoypaulhere@gmail.com>
2024-09-28 19:31:57 +0530

    Avrage slaary

---
+++ week 3/`491-avg salary.py
@@ -0,0 +1,32 @@
+'''
+Example 1:
+
+Input: salary = [4000,3000,1000,2000]
+Output: 2500.00000
+Explanation: Minimum salary and maximum salary are 1000 and 4000 respectively.
+Average salary excluding minimum and maximum salary is (2000+3000) / 2 = 2500
+
+'''
+
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        
+        total = 0
+        mini = float('inf')
+        maxi = float('-inf')
+        
+        for num in salary:
+            if num < mini:
+                mini = num
+        
+        for num in salary:
+            if num > maxi:
+                maxi = num
+        
+
+        for i in salary:
+            total += i
+        
+        total = total-(mini+maxi)
+        avg = total / (len(salary)-2)
+        return avg
