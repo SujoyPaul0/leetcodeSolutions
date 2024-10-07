580ee472af0fa4ce718f6e7481898f2862bde4e5
Sujoy Paul <sujoypaulhere@gmail.com>
2024-10-07 11:18:24 +0530

    Generate Parentheses

---
+++ Stack/22 Generate Parentheses.py
@@ -0,0 +1,35 @@
+'''
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+Input: n = 1
+Output: ["()"]
+'''
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # only add open paranthesis if open < n
+        # only add a closing paranthesis if closed < open
+        # valid IIF open == closed == n
+
+        stack = []
+        res = []
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+            
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+            
+        backtrack(0, 0)
+        return res
+         
