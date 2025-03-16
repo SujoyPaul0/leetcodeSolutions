1cb219c007eaa47982fef3c2a898c33d5d146829
Sujoy Paul <sujoypaulhere@gmail.com>
2025-03-16 23:16:21 +0530

    rotated array

---
+++ Array and hashing/189-Rotated-Array.py
@@ -0,0 +1,24 @@
+'''
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+'''
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+        rotated = [0] * n
+
+        for i in range(n):
+            rotated[(i + k) % n] = nums[i]
+
+        for i in range(n):
+            nums[i] = rotated[i]
