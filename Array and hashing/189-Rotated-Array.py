2a481d308783aee6a10ac9a98414b1dda249f535
Sujoy Paul <sujoypaulhere@gmail.com>
2025-03-16 23:17:09 +0530

    current position

--- Array and hashing/189-Rotated-Array.py
+++ Array and hashing/189-Rotated-Array.py
@@ -7,6 +7,8 @@ Explanation:
 rotate 1 steps to the right: [7,1,2,3,4,5,6]
 rotate 2 steps to the right: [6,7,1,2,3,4,5]
 rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+(current position + remainder) % length of input array
 '''
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
