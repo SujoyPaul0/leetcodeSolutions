ef7dc792bc652b9fb98cb14eaecab3fcbfd808a3
Sujoy Paul <sujoypaulhere@gmail.com>
2025-03-16 22:18:37 +0530

    majority elements

---
+++ Array and hashing/169-Majority-elements.py
@@ -0,0 +1,21 @@
+'''
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+'''
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        m = defaultdict(int)
+
+        for num in nums:
+            m[num] += 1
+        n = n // 2
+        for key, value in m.items():
+            if value > n:
+                return key        
