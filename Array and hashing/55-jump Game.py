a3f950994a22e6a83c4af8083b802f75603d5f18
Sujoy Paul <sujoypaulhere@gmail.com>
2025-03-17 22:44:49 +0530

    jump game

---
+++ Array and hashing/55-jump Game.py
@@ -0,0 +1,23 @@
+'''
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+'''
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        gas = 0
+        for n in nums:
+            if gas < 0:
+                return False
+            elif n > gas:
+                gas = n
+            gas -= 1
+        
+        return True
