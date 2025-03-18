27615e7c5b0e52260d860aaf399c02b6175900a8
Sujoy Paul <sujoypaulhere@gmail.com>
2025-03-18 21:11:23 +0530

    jump game ii

---
+++ Array and hashing/122-jump-game-ii.py
@@ -0,0 +1,25 @@
+'''
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [2,3,0,1,4]
+Output: 2
+'''
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        near = far = jumps = 0
+
+        while far < len(nums) - 1:
+            farthest = 0
+            for i in range(near, far + 1):
+                farthest = max(farthest, i + nums[i])
+
+            near = far + 1
+            far = farthest
+            jumps += 1
+        
+        return jumps
