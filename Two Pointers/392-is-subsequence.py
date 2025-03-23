091b7d3153397178dc7d3ce4c22fec96d1c6bd8f
Sujoy Paul <sujoypaulhere@gmail.com>
2025-03-24 00:28:09 +0530

    is subsequence

---
+++ Two Pointers/392-is-subsequence.py
@@ -0,0 +1,18 @@
+'''
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+'''
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return i == len(s)
