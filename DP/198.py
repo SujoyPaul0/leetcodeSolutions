7f4125ce977fce5c365d29b25b099cce9416011f
Sujoy Paul <sujoypaulhere@gmail.com>
2025-07-11 18:47:24 +0530

    House Robber

---
+++ DP/198.py
@@ -0,0 +1,16 @@
+'''
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4
+'''
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        
+        return rob2
