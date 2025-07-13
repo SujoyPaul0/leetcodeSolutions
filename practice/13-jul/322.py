9f280e435db055a8dbd5cf6a4454b7e2c8d2e418
Sujoy Paul <149752139+SujoyPaul0@users.noreply.github.com>
2025-07-13 23:50:41 +0530

    Practice

---
+++ practice/13-jul/322.py
@@ -0,0 +1,20 @@
+'''
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+
+Input: coins = [2], amount = 3
+Output: -1
+'''
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for a in range(amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
+
+        return dp[amount] if dp[amount] != amount + 1 else -1
